316e585d113b2f44e781cbc48c9b19e409415440
FernandoPinto <1120364@isep.ipp.pt>
2018-04-01 14:49:19 +0100

    Added welcome page.

---
+++ src/components/Welcome.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    
+    <v-jumbotron :gradient="gradient" dark>
+      <v-container fill-height>
+        <v-layout align-center>
+          <v-flex text-xs-center>
+            <h1>{{ msg }}</h1>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+  data () {
+    return {
+      msg: 'Welcome to Promodoro',
+      gradient: 'to top, #42b983, #1b4e37'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Timer from '@/components/Timer'
 import History from '@/components/History'
+import Welcome from '@/components/Welcome'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      name: 'Welcome',
+      component: Welcome
+    },
     {
       path: '/timer',
       name: 'Timer',
